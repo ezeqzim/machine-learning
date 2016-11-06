e252f6cfede99b48600b2bc5f97c1bd3c8dfc288
Ezequiel Zimenspitz <ezeqzim@gmail.com>
2016-11-05 21:57:31 -0300

    tp2 estable

--- TP2/tp2.py
+++ TP2/tp2.py
@@ -6,10 +6,10 @@ class Board:
     self.row = row
     self.col = col
     self.available_moves = [0] * col
-    self.board = [' '] * (row * col)
+    self.state = [' '] * (col * row)
 
   def move(self, col, char):
-    self.board[self.transform_move(self.available_moves[col], col)] = char
+    self.state[self.transform_move(self.available_moves[col], col)] = char
     self.available_moves[col] += 1
 
   def transform_move(self, row, col):
@@ -19,10 +19,41 @@ class Board:
     return [(self.available_moves[i], i) for i in xrange(0, self.col) if self.available_moves[i] < self.row]
 
   def player_wins(self, char):
+    for row in xrange(self.row - 1, -1, -1):
+      for col in xrange(0, self.col - 3):
+        if ((self.state[self.transform_move(row, col)] == char) and (self.state[self.transform_move(row, col + 1)] == char) and (self.state[self.transform_move(row, col + 2)] == char) and (self.state[self.transform_move(row, col + 3)] == char)):
+          return True
+
+    for col in xrange(0, self.col):
+      for row in xrange(self.row - 1, 2, -1):
+        move = self.transform_move(row, col)
+        if ((self.state[self.transform_move(row, col)] == char) and (self.state[self.transform_move(row - 1, col)] == char) and (self.state[self.transform_move(row - 2, col)] == char) and (self.state[self.transform_move(row - 3, col)] == char)):
+          return True
+
+    for row in xrange(self.row - 1, 2, -1):
+      for col in xrange(0, self.col - 3):
+        move = self.transform_move(row, col)
+        if ((self.state[self.transform_move(row, col)] == char) and (self.state[self.transform_move(row - 1, col + 1)] == char) and (self.state[self.transform_move(row - 2, col + 2)] == char) and (self.state[self.transform_move(row - 3, col + 3)] == char)):
+          return True
+
+    for row in xrange(self.row - 4, -1, -1):
+      for col in xrange(0, self.col - 3):
+        move = self.transform_move(row, col)
+        if ((self.state[self.transform_move(row, col)] == char) and (self.state[self.transform_move(row + 1, col + 1)] == char) and (self.state[self.transform_move(row + 2, col + 2)] == char) and (self.state[self.transform_move(row + 3, col + 3)] == char)):
+          return True
     return False
 
   def board_full(self):
     return len(self.available_moves_filtered()) == 0
+
+  def display_board(self):
+    row = '| {} |'
+    hr = '-----'
+    print ((row * self.col + '\n' + hr * self.col + '\n') * self.row).format(*reversed(self.state))
+
+  def illegal_move(self, col):
+    bad = col < 0 or col >= self.col
+    return bad or self.available_moves[col] == self.row
 
 class FourInARow:
   def __init__(self, playerX, playerO, row, col):
@@ -33,20 +64,25 @@ class FourInARow:
   def play_game(self):
     self.playerX.start_game('X', self.board)
     self.playerO.start_game('O', self.board)
-    while True: #yolo
+    while True:
       if self.playerX_turn:
-        player, char, other_player = self.playerX, 'X', self.playerO
+        player = self.playerX
+        char = 'X'
+        other_player = self.playerO
       else:
-        player, char, other_player = self.playerO, 'O', self.playerX
+        player = self.playerO
+        char = 'O'
+        other_player = self.playerX
 
-      if player.breed == "human":
-        self.display_board()
+      if player.breed == 'human':
+        self.board.display_board()
 
       move = player.move(self.board) #(row, col)
 
-      # if self.board[space-1] != ' ': # illegal move
-      #   player.reward(-99, self.board.board) # score of shame
-      #   break
+      if move is None: # illegal move ONLY FOR REAL LIFE PLAYER
+        player.reward(-99, self.board) # score of shame
+        break
+
       self.board.move(move[1], char)
 
       if self.board.player_wins(char):
@@ -62,32 +98,31 @@ class FourInARow:
       other_player.reward(0, self.board)
       self.playerX_turn = not self.playerX_turn
 
-  def display_board(self):
-    row = " {} | {} | {}"
-    hr = "\n-----------\n"
-    print (row + hr + row + hr + row).format(*self.board)
-
 class Player(object):
   def __init__(self):
-    self.breed = "human"
+    self.breed = 'human'
 
-  def start_game(self, char):
-    print "\nNew game!"
+  def start_game(self, char, board):
+    print '\nNew game!'
 
   def move(self, board):
-    return int(raw_input("Your move? "))
+    col = int(raw_input('Your move (1 to {board.col})? '))
+    col = board.col - col
+    if board.illegal_move(col):
+      return None
+    return (board.available_moves[col], col)
 
   def reward(self, value, board):
-    print "{} rewarded: {}".format(self.breed, value)
+    print '{} rewarded: {}'.format(self.breed, value)
 
 class RandomPlayer(Player):
   def __init__(self):
-    self.breed = "random"
+    self.breed = 'random'
 
   def reward(self, value, board):
     pass
 
-  def start_game(self, char):
+  def start_game(self, char, board):
     pass
 
   def move(self, board):
@@ -95,7 +130,7 @@ class RandomPlayer(Player):
 
 class QLearningPlayer(Player):
   def __init__(self, epsilon=0.2, alpha=0.3, gamma=0.9):
-    self.breed = "Qlearner"
+    self.breed = 'Qlearner'
     self.harm_humans = True # Fuck Asimov
     self.q = {} # (state, action) keys: Q values
     self.epsilon = epsilon # e-greedy chance of random exploration
@@ -103,14 +138,14 @@ class QLearningPlayer(Player):
     self.gamma = gamma # discount factor for future rewards
 
   def start_game(self, char, board):
-    self.last_board = board
+    self.last_board = copy.deepcopy(board)
     self.last_move = None
 
   def getQ(self, state, action):
-    # encourage exploration; "optimistic" 1.0 initial values
-    if self.q.get((tuple(state), action)) is None:
-        self.q[(tuple(state), action)] = 1.0 # Esto se puede modificar!!
-    return self.q.get((tuple(state), action))
+    # encourage exploration; 'optimistic' 1.0 initial values
+    if self.q.get((tuple(state), self.last_board.transform_move(action[0], action[1]))) is None:
+        self.q[(tuple(state), self.last_board.transform_move(action[0], action[1]))] = 1.0 # Esto se puede modificar!!
+    return self.q.get((tuple(state), self.last_board.transform_move(action[0], action[1])))
 
   def move(self, board):
     self.last_board = copy.deepcopy(board)
@@ -120,7 +155,7 @@ class QLearningPlayer(Player):
       self.last_move = random.choice(actions)
       return self.last_move
 
-    qs = [self.getQ(self.last_board.board, a) for a in actions]
+    qs = [self.getQ(self.last_board.state, a) for a in actions]
     maxQ = max(qs)
 
     if qs.count(maxQ) > 1:
@@ -137,21 +172,23 @@ class QLearningPlayer(Player):
     if self.last_move:
       self.learn(self.last_board, self.last_move, value, board)
 
-  def learn(self, board_state, action, reward, board_result_state):
-    prev = self.getQ(board_state, action)
-    maxqnew = max([self.getQ(board_result_state.board, a) for a in board_state.available_moves_filtered()])
-    self.q[(tuple(state), action)] = prev + self.alpha * ((reward + self.gamma * maxqnew) - prev)
+  def learn(self, board, action, reward, board_result):
+    prev = self.getQ(board.state, action)
+    qs = [self.getQ(board_result.state, a) for a in board_result.available_moves_filtered()]
+    if not len(qs) == 0:
+      maxqnew = max(qs)
+      self.q[(tuple(board.state), action)] = prev + self.alpha * (reward + self.gamma * maxqnew - prev)
 
 p1 = QLearningPlayer()
 p2 = QLearningPlayer()
 
-for i in xrange(0,2):
+for i in xrange(0, 1000000):
   t = FourInARow(p1, p2, 6, 7)
   t.play_game()
 
-#p1 = Player()
-#p2.epsilon = 0
+p1 = Player()
+p2.epsilon = 0
 
-#while True:
-#    t = FourInARow(p1, p2, 6, 7)
-#    t.play_game()
+while True:
+   t = FourInARow(p1, p2, 6, 7)
+   t.play_game()
