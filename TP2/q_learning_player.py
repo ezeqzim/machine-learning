93153f79d8b2ae178b41b4ba0573fd942b84eff5
Ezequiel Zimenspitz <ezeqzim@gmail.com>
2016-11-16 11:24:21 -0300

    exp1 informe empezado

--- TP2/q_learning_player.py
+++ TP2/q_learning_player.py
@@ -18,7 +18,7 @@ class QLearningPlayer(Player):
   def getQ(self, state, action):
     # encourage exploration; 'optimistic' 1.0 initial values
     if self.q.get((tuple(state), action)) is None:
-        self.q[(tuple(state), action)] = 1.0 # Esto se puede modificar!!
+        self.q[(tuple(state), action)] = 0.0 # Esto se puede modificar!!
     return self.q.get((tuple(state), action))
 
   def move(self, board):
